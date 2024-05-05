53e0684a9a5b86b7de4f4118e5f64b9f395c65e7
OoftyGoofty <166545369+OoftyGoofty@users.noreply.github.com>
2024-05-05 21:48:56 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,66 +24,48 @@ vrcholy_index = {
 }
 
 graf = {
-  'A': {'B': 2, 'D': 6},
+  'A': {'B': 2, 'D': 6, 'E': 2},
   'B': {'A': 2, 'D': 3, 'E': 6},
   'C': {'D': 1},
   'D': {'A': 6, 'B': 3, 'C': 1, 'E': 2},
-  'E': {'B': 6, 'D': 2},
+  'E': {'A': 2, 'B': 6, 'D': 2},
 }
 
-#cesty = cesty(vrcholy, 'A')
-
-cesty = [['A', 'B', 'C', 'D', 'E', 'A'],
-         ['A', 'B', 'C', 'E', 'D', 'A']]
-
+cesty = cesty(vrcholy, 'A')
 vzdalenosti_a_cesty = Dijikstra.vzdalenostiACesty(graf)
 obchodni_cesty = {}
-
-#for line in vzdalenosti_a_cesty:
-#    print(line, "\n")
-#for line in cesty:
-#    print(line, "\n")
 
 for cesta in cesty:
     trasa_cesty = []
     cena_cesty = 0
     
     i = 1
-    while i < len(cesta): # 0 - 5
+    while i < len(cesta):
         
-        #if cesta[i] not in trasa_cesty or cesta[i] == cesta[0]:
-        ceny_a_cesty_z_vrcholu = vzdalenosti_a_cesty[vrcholy_index[cesta[i - 1]]] # {'A': (0, ['A']), 'B': (2, ['A', 'B']), 'C': (6, ['A', 'B', 'D', 'C']), 'D': (5, ['A', 'B', 'D']), 'E': (7, ['A', 'B', 'D', 'E'])}
+        ceny_a_cesty_z_vrcholu = vzdalenosti_a_cesty[vrcholy_index[cesta[i - 1]]]
+        vrchol = ceny_a_cesty_z_vrcholu[cesta[i]] 
         
-        #print(ceny_a_cesty_z_vrcholu)
+        cena_cesty = cena_cesty + vrchol[0] 
+        trasa_cesty.extend(vrchol[1]) 
         
-        #for item in ceny_a_cesty_z_vrcholu:
-        #    print(ceny_a_cesty_z_vrcholu[item], "\n")
+        x = i
+        while x < len(cesta):
+
+            if cesta[x] in trasa_cesty and cesta[x] != cesta[i] and cesta[x] != cesta[-1]:
+                cesta.pop(x)
+            x += 1
         
-        vrchol = ceny_a_cesty_z_vrcholu[cesta[i]] # (2, ['A', 'B'])
-        
-        #print(vrchol, "\n")
-        
-        cena_cesty = cena_cesty + vrchol[0] # 2 = 0 + 2
-        trasa_cesty.extend(vrchol[1]) # ['A', 'B']
-        
-        for x in range(i, len(cesta)):
-            print(x)
-        #    if cesta[x] in trasa_cesty and cesta[x] != cesta[-1]:
-        #       cesta.pop(x)
-        #        cesta.insert(0, 'X')
-        
-        print(cesta)
-        
-        #print(i, "\n")
-            
-        #else:
-        #    cesta.pop(i)
         i += 1
-    
-    if cena_cesty in obchodni_cesty:        
-        obchodni_cesty[cena_cesty] = [obchodni_cesty[cena_cesty], trasa_cesty]
-    else:
-        obchodni_cesty[cena_cesty] = trasa_cesty
+
+    y = 1
+    while y < len(trasa_cesty):
+        if trasa_cesty[y] == trasa_cesty[y - 1]:
+            trasa_cesty.pop(y)
+        y += 1
+        
+    key = cena_cesty
+    obchodni_cesty.setdefault(key, [])
+    obchodni_cesty[cena_cesty].append(trasa_cesty)
 
         
 for line in obchodni_cesty:
